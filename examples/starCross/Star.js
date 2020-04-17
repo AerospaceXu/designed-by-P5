cf1b749364406135a5cd39020257901bdbf63897
amazingxhy <610825756@qq.com>
2020-04-18 01:03:54 +0800

    修改bug

--- examples/starCross/Star.js
+++ examples/starCross/Star.js
@@ -1,39 +1,39 @@
 class Star {
-  constructor() {
-    this.x = random(-width / 2, width / 2);
-    this.y = random(-width / 2, width / 2);
-    this.z = random(0, width);
-    
-    this.r = 25 + random(-2, 3);
-    this.isMiss = false;
-    this.sx;
-    this.sy;
-  }
+	constructor() {
+		this.x = random(-width / 2, width / 2)
+		this.y = random(-width / 2, width / 2)
+		this.z = random(0, width)
 
-  update(speed) {
-    this.z -= speed;
-    if (this.z <= 1) {
-      this.x = random(-width / 2, width / 2);
-      this.y = random(-width / 2, width / 2);
-      this.z = width;
-      this.isMiss = false;
-    }
-  }
+		this.r = 25 + random(-2, 3)
+		this.isMiss = false
+		this.sx
+		this.sy
+	}
 
-  show() {
-    fill(255);
-    noStroke();
-    let nowX = map(this.x / this.z, -1, 1, -width / 2, width / 2);
-    let nowY = map(this.y / this.z, -1, 1, -width / 2, width / 2);
-    if (!this.isMiss) {
-      this.sx = nowX;
-      this.sy = nowY;
-      this.isMiss = true;
-    }
-    let nowR = map(this.z, 0, width, this.r, 0);
-    ellipse(nowX, nowY, nowR, nowR);
+	update(speed) {
+		this.z -= speed
+		if (this.z <= 1) {
+			this.x = random(-width / 2, width / 2)
+			this.y = random(-width / 2, width / 2)
+			this.z = width
+			this.isMiss = false
+		}
+	}
 
-    stroke(255);
-    triangle(nowX + nowR / 3, nowY + nowR / 3, nowX - nowR / 3, nowY - nowR / 3, this.sx, this.sy);
-  }
+	show() {
+		fill(255)
+		noStroke()
+		const nowX = map(this.x / this.z, -1, 1, -width / 2, width / 2)
+		const nowY = map(this.y / this.z, -1, 1, -width / 2, width / 2)
+		if (!this.isMiss) {
+			this.sx = nowX
+			this.sy = nowY
+			this.isMiss = true
+		}
+		const nowR = map(this.z, 0, width, this.r, 0)
+		ellipse(nowX, nowY, nowR, nowR)
+
+		stroke(255)
+		triangle(nowX + nowR / 3, nowY + nowR / 3, nowX - nowR / 3, nowY - nowR / 3, this.sx, this.sy)
+	}
 }
--- examples/starCross/sketch.js
+++ examples/starCross/sketch.js
@@ -1,23 +1,22 @@
-let stars = [];
+let stars = []
 
-let wid, heig;
+const wid = document.body.offsetWidth
+const heig = document.body.offsetHeight
 
 function setup() {
-  wid = document.body.offsetWidth;
-  heig = document.body.offsetHeight;
-  createCanvas(wid, heig);
-  let starsNumber = parseInt(width * height / 6500)
-  for (let i = 0; i < starsNumber; i++) {
-    let temp = new Star();
-    stars.push(temp);
-  }
+	createCanvas(wid, heig)
+	let starsNumber = parseInt((width * height) / 6500)
+	for (let i = 0; i < starsNumber; i++) {
+		const temp = new Star()
+		stars.push(temp)
+	}
 }
 
 function draw() {
-  translate(wid / 2, heig / 2);
-  background("#000");
-  for (let i = 0; i < stars.length; i++) {
-    stars[i].update(15);
-    stars[i].show();
-  }
+	translate(wid / 2, heig / 2)
+	background('#000')
+	for (let i = 0; i < stars.length; i++) {
+		stars[i].update(15)
+		stars[i].show()
+	}
 }
